18826ca5ab47e1406048fdc53fef84c24ceef98e
Aditya Mishra <adityamishra200111@gmail.com>
2023-10-11 21:28:13 +0530

    initial

---
+++ from.js
@@ -0,0 +1,15 @@
+document.getElementById("myform").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var date = document.getElementById("date").value;
+  var time = document.getElementById("time").value;
+
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Phone:", phone);
+  console.log("Date:", date);
+  console.log("Time:", time);
+});
