470773dc80c863561e2851e695109adb5e4c38bd
Aditya Mishra <adityamishra200111@gmail.com>
2023-10-11 22:19:21 +0530

    storing object

--- from.js
+++ from.js
@@ -1,15 +1,17 @@
-document.getElementById("myform").addEventListener("submit", function (event) {
+const form = document.getElementById("myform");
+
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var phone = document.getElementById("phone").value;
-  var date = document.getElementById("date").value;
-  var time = document.getElementById("time").value;
+  const fd = new FormData(form);
 
-  console.log("Name:", name);
-  console.log("Email:", email);
-  console.log("Phone:", phone);
-  console.log("Date:", date);
-  console.log("Time:", time);
+  const obj = Object.fromEntries(fd);
+
+  let myobj_serialized = JSON.stringify(obj);
+  localStorage.setItem("form", myobj_serialized);
+
+  // console.log(localStorage);
+
+  // let myobj_dserialized = JSON.parse(localStorage.getItem("form"));
+  // console.log(myobj_dserialized);
 });
