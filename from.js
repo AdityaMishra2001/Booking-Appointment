397f8149fe16520f1e882ef69fa8cbe68a9395e4
Aditya Mishra <adityamishra200111@gmail.com>
2023-10-12 15:00:51 +0530

    dilplaying object on screen

--- from.js
+++ from.js
@@ -1,17 +1,49 @@
+function addInput(e) {
+  e.preventDefault();
+
+  //getting user data input
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let phone = document.getElementById("phone");
+  let date = document.getElementById("date");
+  let time = document.getElementById("time");
+
+  if (
+    name.value === "" ||
+    email.value === "" ||
+    phone.value === "" ||
+    date.value === "" ||
+    time.value === ""
+  ) {
+    alert("Please enter all fields");
+  } else {
+    alert("Details saved to local storage");
+    const details = {
+      Name: name.value,
+      Email: email.value,
+      Phone: phone.value,
+      Date: date.value,
+      Time: time.value,
+    };
+
+    //saving to local storage
+    localStorage.setItem(name.value, JSON.stringify(details));
+
+    //display on screen
+    let div = document.createElement("div");
+
+    div.appendChild(document.createTextNode(name.value));
+    div.appendChild(document.createTextNode("🔶" + email.value + "🔶"));
+    div.appendChild(document.createTextNode(phone.value + "🔶"));
+    div.appendChild(document.createTextNode(date.value + " "));
+
+    div.style.fontWeight = "bold";
+    div.style.textAlign = "center";
+    div.style.color = "brown";
+
+    form.after(div);
+  }
+}
+
 const form = document.getElementById("myform");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const fd = new FormData(form);
-
-  const obj = Object.fromEntries(fd);
-
-  let myobj_serialized = JSON.stringify(obj);
-  localStorage.setItem("form", myobj_serialized);
-
-  // console.log(localStorage);
-
-  // let myobj_dserialized = JSON.parse(localStorage.getItem("form"));
-  // console.log(myobj_dserialized);
-});
+form.addEventListener("submit", addInput);
